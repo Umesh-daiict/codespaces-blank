17dab8f7fa12a0de19a9d4fe76c667dc50482f29
Sun-007 <78736365+Umesh-daiict@users.noreply.github.com>
2023-09-21 23:38:08 +0530

    Added closure check it

--- hello.js
+++ hello.js
@@ -1,5 +1,17 @@
 console.log("helllo, world");
 console.log("you are for that only")
 var var1 = "test 000";
-const var1 = "testass" ;
+const var1 = "testass";
 console.log(var1);
+
+const closserFuc = {
+  const cash ={};
+  return (key,value) =>{
+    if(cash[`${key}`]){
+      return cash[`${key}`]
+    }else{
+      cash[`${key}`] = value;
+    }
+  }
+  
+}
